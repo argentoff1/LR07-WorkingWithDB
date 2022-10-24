213eeedf60f74f15a816d7b11a0458344b3d571a
argentoff1 <mparinos56@gmail.com>
2022-10-24 22:22:44 +0300

    мы это с...делали

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,119 +1,137 @@
 package com.example.myapplication;
 
-import static androidx.core.content.PackageManagerCompat.LOG_TAG;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.database.Cursor;
-import android.util.Log;
+import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends Activity implements OnClickListener {
+
+    final String LOG_TAG = "myLogs";
     Button btnAdd, btnRead, btnClear, btnUpd, btnDel;
     EditText etFullName, etRecordingTime, etID;
-
     DBHelper dbHelper;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnAdd.setOnClickListener(this);
-
         btnRead = (Button) findViewById(R.id.btnRead);
         btnRead.setOnClickListener(this);
-
-        btnUpd = (Button) findViewById(R.id.btnUpd);
-        btnUpd.setOnClickListener(this);
-
-        btnDel = (Button) findViewById(R.id.btnDel);
-        btnDel.setOnClickListener(this);
-
         btnClear = (Button) findViewById(R.id.btnClear);
         btnClear.setOnClickListener(this);
-
+        btnUpd = (Button) findViewById(R.id.btnUpd);
+        btnUpd.setOnClickListener(this);
+        btnDel = (Button) findViewById(R.id.btnDel);
+        btnDel.setOnClickListener(this);
         etFullName = (EditText) findViewById(R.id.etFullName);
         etRecordingTime = (EditText) findViewById(R.id.etRecordingTime);
         etID = (EditText) findViewById(R.id.etID);
 
         dbHelper = new DBHelper(this);
     }
-
-    @SuppressLint("RestrictedApi")
     @Override
     public void onClick(View v) {
 
-        String fullName = etFullName.getText().toString();
-        String recordingTime = etRecordingTime.getText().toString();
+        ContentValues cv = new ContentValues();
+
+        String name = etFullName.getText().toString();
+        String email = etRecordingTime.getText().toString();
         String id = etID.getText().toString();
 
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-
-        ContentValues contentValues = new ContentValues();
-
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
         switch (v.getId()) {
             case R.id.btnAdd:
-                contentValues.put(DBHelper.KEY_NAME, fullName);
-                contentValues.put(DBHelper.KEY_MAIL, recordingTime);
+                Log.d(LOG_TAG, "--- Insert in mytable: ---");
+                cv.put("name", name);
+                cv.put("email", email);
 
-                database.insert(DBHelper.TABLE_CONTACTS, null, contentValues);
+                long rowID = db.insert("mytable", null, cv);
+                Log.d(LOG_TAG, "row inserted, ID = " + rowID);
                 break;
-
             case R.id.btnRead:
-                Cursor cursor = database.query(DBHelper.TABLE_CONTACTS, null, null, null, null, null, null);
+                Log.d(LOG_TAG, "--- Rows in mytable: ---");
 
-                if (cursor.moveToFirst()) {
-                    int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME);
-                    int emailIndex = cursor.getColumnIndex(DBHelper.KEY_MAIL);
+                Cursor c = db.query("mytable", null, null, null, null,
+                        null, null);
+
+                if (c.moveToFirst()) {
+
+                    int idColIndex = c.getColumnIndex("id");
+                    int nameColIndex = c.getColumnIndex("name");
+                    int emailColIndex = c.getColumnIndex("email");
                     do {
-                        Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
-                                ", name - " + cursor.getString(nameIndex) +
-                                ", Recording Time = " + cursor.getString(emailIndex));
-                    } while (cursor.moveToNext());
-                } else
-                    Log.d("mLog", "o rows");
 
-                cursor.close();
+                        Log.d(LOG_TAG,
+                                "ID = " + c.getInt(idColIndex) + ", name = "
+                                        + c.getString(nameColIndex) + ", email = "
+                                        + c.getString(emailColIndex));
+
+
+                    } while (c.moveToNext());
+                } else
+                    Log.d(LOG_TAG, "0 rows");
+                c.close();
                 break;
-            // https://startandroid.ru/ru/uroki/vse-uroki-spiskom/75-urok-35-metody-query-i-delete-s-ukazaniem-uslovija.html
+            case R.id.btnClear:
+                Log.d(LOG_TAG, "--- Clear mytable: ---");
+
+                int clearCount = db.delete("mytable", null, null);
+                Log.d(LOG_TAG, "deleted rows count = " + clearCount);
+                break;
             case R.id.btnUpd:
                 if (id.equalsIgnoreCase("")) {
                     break;
                 }
                 Log.d(LOG_TAG, "--- Update mytable: ---");
-                // подготовим значения для обновления
-                contentValues.put(DBHelper.KEY_ID, id);
-                contentValues.put(DBHelper.KEY_NAME, fullName);
-                contentValues.put(DBHelper.KEY_MAIL, recordingTime);
-                // обновляем по id
-                int updCount = database.update("contacts", contentValues, "id = ?",
+
+                cv.put("name", name);
+                cv.put("email", email);
+
+                int updCount = db.update("mytable", cv, "id = ?",
                         new String[] { id });
                 Log.d(LOG_TAG, "updated rows count = " + updCount);
                 break;
-
             case R.id.btnDel:
                 if (id.equalsIgnoreCase("")) {
                     break;
                 }
                 Log.d(LOG_TAG, "--- Delete from mytable: ---");
-                // удаляем по id
-                int delCount = database.delete("mytable", "id = " + id, null);
+
+                int delCount = db.delete("mytable", "id = " + id,
+                        null);
                 Log.d(LOG_TAG, "deleted rows count = " + delCount);
                 break;
-
-            case R.id.btnClear:
-                database.delete(DBHelper.TABLE_CONTACTS, null, null);
-                break;
         }
+
         dbHelper.close();
+    }
+    class DBHelper extends SQLiteOpenHelper {
+        public DBHelper(Context context) {
+
+            super(context, "myDB", null, 1);
+        }
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+
+            db.execSQL("create table mytable ("
+                    + "id integer primary key autoincrement,"
+                    + "name text,"
+                    + "email text" + ");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int
+                newVersion) {
+        }
     }
 }
